caf622bba1df6bd445ba3a2ec27ab338d48d6c8f
Leo Folsom <leoebfolsom@gmail.com>
2023-10-10 21:02:16 -0700

    remove duplicate column

--- dags/datafold_mwaa.py
+++ dags/datafold_mwaa.py
@@ -36,7 +36,6 @@ CREATE_BEARS_TABLE = """
     CREATE OR REPLACE TRANSIENT TABLE DATAFOLD_AIRFLOW{{ params.schema_name_postfix }}.{{ params.table_name }}
         (
             id INT,
-            name INT,
             name VARCHAR(25),
             species VARCHAR(25),
             weight INT,
