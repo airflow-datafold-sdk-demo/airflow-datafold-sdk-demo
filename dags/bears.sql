34c454df55b6cb98c1c17de1f2d8038bde59a3cb
Leo Folsom <leoebfolsom@gmail.com>
2023-10-12 12:29:10 -0700

    add a row and change a couple things

--- dags/bears.sql
+++ dags/bears.sql
@@ -15,11 +15,12 @@ INSERT INTO DATAFOLD_AIRFLOW{{ params.schema_name_postfix }}.{{ params.table_nam
 (id, name, species, weight, length, hobby)
 VALUES
 (1, 'Bruno', 'American Black Bear', 300, 6.5, 'Fishing'),
-(2, 'Honey', 'Grizzly Bear', 900, 8.2, 'Hiking'),
+(2, 'Honey', 'Grizzly Bear', 901, 8.2, 'Hiking'),
 (3, 'Teddy', 'Polar Bear', 1100, 9.5, 'Swimming'),
 (4, 'Cubby', 'Brown Bear', 700, 7.9, 'Berry Picking'),
 (5, 'Munch', 'Kodiak Bear', 1500, 9.0, 'Salmon Fishing'),
-(6, 'BamBam', 'Panda Bear', 300, 5.5, 'Bamboo Eating'),
+(6, 'BamBam', 'Panda Bear', 300, 5.6, 'Bamboo Eating'),
 (7, 'Smokey', 'Sloth Bear', 250, 6.2, 'Ant Eating'),
 (8, 'Sunny', 'Sun Bear', 100, 4.3, 'Tree Climbing'),
+(9, 'Rabbit', 'Rabbit Bear', 300, 6.3, 'Hopping'),
 (10, 'Specs', 'Spectacled Bear', 200, 5.7, 'Plant Eating');
