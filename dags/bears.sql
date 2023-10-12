5c68fdbec48f283986bfb55b0c6adf5ef13a4154
Leo Folsom <leoebfolsom@gmail.com>
2023-10-12 00:04:14 -0700

    change some column names

--- dags/bears.sql
+++ dags/bears.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE TRANSIENT TABLE DATAFOLD_AIRFLOW{{ params.schema_name_postfix }}.{{ params.table_name }}
         (
             id INT,  
-            leo VARCHAR(25),
+            jeremy VARCHAR(25),
             species VARCHAR(25),
             weight INT,
             length FLOAT,
