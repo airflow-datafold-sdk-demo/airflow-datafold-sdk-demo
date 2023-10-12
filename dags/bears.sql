2c462c5bd11e154b0dc5de202f17e3305b49d099
Leo Folsom <leoebfolsom@gmail.com>
2023-10-12 00:23:28 -0700

    fix error

--- dags/bears.sql
+++ dags/bears.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE TRANSIENT TABLE DATAFOLD_AIRFLOW{{ params.schema_name_postfix }}.{{ params.table_name }}
         (
             id INT,  
-            jeremy VARCHAR(25),
+            name VARCHAR(25),
             species VARCHAR(25),
             weight INT,
             length FLOAT,
