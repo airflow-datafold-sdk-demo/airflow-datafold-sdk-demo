2ede66b5fafe83940361ff14b6ee64db934e38d8
Leo Folsom <leoebfolsom@gmail.com>
2023-10-28 00:10:38 -0700

    mon --> tues

--- dags/forestfires.sql
+++ dags/forestfires.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE TRANSIENT TABLE DATAFOLD_AIRFLOW{{ params.schema_name_postfix 
         (
             id INT, 
             jeremy INT,
-            mon VARCHAR(25), 
+            tues VARCHAR(25), 
             day VARCHAR(25),
             ffmc FLOAT,
             dmc FLOAT,
