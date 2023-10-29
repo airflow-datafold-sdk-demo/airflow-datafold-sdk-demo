93f7aaca25c752891996bdccfbfa46f5e17ea222
Leo Folsom <leoebfolsom@gmail.com>
2023-10-28 22:24:14 -0700

    add insert into statement

--- dags/top_squirrels.sql
+++ dags/top_squirrels.sql
@@ -5,6 +5,7 @@ CREATE OR REPLACE TRANSIENT TABLE DATAFOLD_AIRFLOW{{ params.schema_name_postfix 
 
 TRUNCATE table DATAFOLD_AIRFLOW{{ params.schema_name_postfix }}.{{ params.table_name }};
 
+INSERT INTO DATAFOLD_AIRFLOW{{ params.schema_name_postfix }}.{{ params.table_name }} (id)
 SELECT id FROM DATAFOLD_AIRFLOW{{ params.schema_name_postfix }}.SQUIRRELS
 ORDER BY id DESC
 LIMIT 5;
